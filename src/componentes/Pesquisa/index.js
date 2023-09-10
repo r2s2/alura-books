eeac8f74b9614119dad8d22b67377b16f458f221
r2s2 <rodrigo.sacramento@gmail.com>
2023-09-10 11:16:32 -0300

    Ultimos lançamentos

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,6 +1,8 @@
 import Input from '../Input'
 import styled from 'styled-components'
 import { useState } from 'react'
+import { livros } from './dadosPesquisa'
+
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -24,16 +26,51 @@ const Subtitulo = styled.h3`
     margin-bottom: 40px;
 `
 
+const Resultado = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+
+    p{
+        width: 200px;
+    }
+
+    img {
+        width: 100px;
+    }
+
+    &:hover {
+        border: 1px solid white;
+    }
+`
+
 function Pesquisa() {
-    const [textoDigitado, setTextoDigitado] = useState('')
-
-
+    const [livrosPesquisados, setLivrosPesquisados] = useState([])
+    
     return (
         <PesquisaContainer>
             <Titulo>Já sabe por onde começar? </Titulo>
             <Subtitulo> Encontre seu livro em nossa estante.</Subtitulo>
-            <Input placeholder='Escreva sua própria leitura' onBlur={evento => setTextoDigitado(evento.target.value)}></Input>
-            <p>{textoDigitado}</p>
+            <Input placeholder='Escreva sua própria leitura' onBlur={evento => {
+                const textoDigitado = evento.target.value
+                const resultadoPesquisa = livros.filter(livro => livro.nome.includes (textoDigitado))
+                setLivrosPesquisados(resultadoPesquisa)                
+                }}>
+
+            </Input>
+
+            {livrosPesquisados.map(livro => (
+                <Resultado>
+                    
+                    <img src={ livro.src } alt= 'capa do livro'/>
+                    <p>{ livro.nome } </p>
+           
+
+                </Resultado>
+            ))}
+            
         </PesquisaContainer>
         
     )
